6706b9180c92239cd4591bc0368e74d16007d757
kevinrobinson <kevin.robinson.0@gmail.com>
2019-05-03 09:30:01 -0400

    Pull functions out from inside train()

--- js/linclass2.js
+++ js/linclass2.js
@@ -1,45 +1,46 @@
 (function() {
 
-// Create model
-const model = tf.sequential({
-  layers: [
-    tf.layers.dense({inputShape: [9], units: 10, activation: 'relu'}),
-    tf.layers.dense({units: 10, activation: 'relu'}),
-    tf.layers.dense({units: 3, activation: 'softmax'}),
-  ]
-});
+  // Create model
+  const model = tf.sequential({
+    layers: [
+      tf.layers.dense({inputShape: [9], units: 10, activation: 'relu'}),
+      tf.layers.dense({units: 10, activation: 'relu'}),
+      tf.layers.dense({units: 3, activation: 'softmax'}),
+    ]
+  });
 
-model.compile({
-  optimizer: 'sgd',
-  loss: 'categoricalCrossentropy',
-  metrics: ['accuracy']
-});
-
-// Train for 20 epochs with batch size 16
-function onBatchEnd(batch, logs) {
-  console.log('Accuracy', logs.acc);
-}
-
-function train() {
-  // Get data
-  // TODO: when train button is clicked, these should be filled entirely (not just
-  // one example)
-  // const trainData = window.lastDataset
-  const trainX = tf.tensor(window.lastX)     // Must be shape [num_examples, 9]
-  const trainY = tf.tensor(window.lastY)     // Must be shape [num_examples, 3] (one-hot)
-  console.log(trainX)
-  console.log(trainY)
-
-  model.fit(trainX, trainY, {
-    epochs: 50,
-    batchSize: 16,
-    callbacks: {onBatchEnd}
-  }).then(info => {
-    console.log('Final accuracy', info.history.acc);
+  model.compile({
+    optimizer: 'sgd',
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy']
   });
 
   // warm-up
   tf.tidy(() => model.predict(tf.zeros([1, 9])));
+
+  // Train for 20 epochs with batch size 16
+  function onBatchEnd(batch, logs) {
+    console.log('Accuracy', logs.acc);
+  }
+
+  function train() {
+    // Get data
+    // TODO: when train button is clicked, these should be filled entirely (not just
+    // one example)
+    // const trainData = window.lastDataset
+    const trainX = tf.tensor(window.lastX)     // Must be shape [num_examples, 9]
+    const trainY = tf.tensor(window.lastY)     // Must be shape [num_examples, 3] (one-hot)
+    console.log(trainX)
+    console.log(trainY)
+
+    model.fit(trainX, trainY, {
+      epochs: 50,
+      batchSize: 16,
+      callbacks: {onBatchEnd}
+    }).then(info => {
+      console.log('Final accuracy', info.history.acc);
+    });
+  }
 
   // Train for 20 epochs with batch size 16
   function onBatchEnd(batch, logs) {
@@ -96,5 +97,4 @@ function train() {
     init: init,
     predict: predict
   };
-}
 })();
